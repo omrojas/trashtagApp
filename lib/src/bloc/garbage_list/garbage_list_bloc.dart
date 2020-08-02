b4cddfe1b3eb7e131c64d829b623f88beb6251b8
Hector Lemus <dev.hectorlemus@gmail.com>
2020-08-02 14:04:46 -0500

    Only garbage collections with quantities greater than zero are sent.

--- lib/src/bloc/garbage_list/garbage_list_bloc.dart
+++ lib/src/bloc/garbage_list/garbage_list_bloc.dart
@@ -31,21 +31,24 @@ class GarbageListBloc extends Bloc<GarbageListEvent, GarbageListState> {
     if (event is SubmitList) {
       yield SendingCollect();
       final errorMessage = 'We had a problem trying to save your items.';
+      final succesfulMessage =
+          'Your list has been submitted for approvall. We will check your work in order to create an official TrashTag report with it';
 
       try {
-        final status = await repository.submitCollect(
-          selectedTrashes: _trashes,
-        );
-        if (status) {
-          final succesfulMessage =
-              'Your list has been submitted for approvall. We will check your work in order to create an official TrashTag report with it';
-          _trashes.clear();
-          yield SuccessfulSend(message: succesfulMessage);
-        } else {
-          yield SendError(error: errorMessage);
+        final trashes = _trashes.where((value) => value.quantity > 0).toList();
+        if (trashes.isNotEmpty) {
+          final status = await repository.submitCollect(
+            selectedTrashes: trashes,
+          );
+
+          if (status) {
+            _trashes.clear();
+            yield SuccessfulSend(message: succesfulMessage);
+          } else {
+            yield SendError(error: errorMessage);
+          }
         }
       } catch (e) {
-        print(e);
         yield SendError(error: errorMessage);
       }
     }
