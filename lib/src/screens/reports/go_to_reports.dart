31c2af39100817213e93a6435141c749102716c9
Hector Lemus <dev.hectorlemus@gmail.com>
2020-08-26 14:40:31 -0500

    'LinkButton' is implemented.

--- lib/src/screens/reports/go_to_reports.dart
+++ lib/src/screens/reports/go_to_reports.dart
@@ -1,24 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:trashtagApp/src/bloc/reports/reports_bloc.dart';
+import 'package:trashtagApp/src/widgets/link_button.dart';
 
 class GoToReports extends StatelessWidget {
   const GoToReports({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FlatButton(
+    return LinkButton(
+      text: 'Go back to Reports',
       onPressed: () {
         BlocProvider.of<ReportsBloc>(context).add(
           BackButtonPressed(),
         );
       },
-      child: Text(
-        'Go back to Reports',
-        style: TextStyle(
-          color: Theme.of(context).primaryColor,
-        ),
-      ),
     );
   }
 }
